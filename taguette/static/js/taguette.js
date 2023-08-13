84316f4dc4a2768c290f0cdfeb581a3723ae199a
name <email@email.com>
2023-08-13 08:19:27 -0300

    add total doc

--- taguette/static/js/taguette.js
+++ taguette/static/js/taguette.js
@@ -568,11 +568,12 @@ function updateDocumentsList() {
     if(doc.id === current_document) {
       elem.classList.add('document-link-current');
     }
+
     elem.innerHTML =
      '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">'+
      '<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />'+
    '</svg>'+
-
+   
      '<p class="text-xs text-gray-900 dark:text-white">'+ doc.description +'</p>'+
     '<div class="flex justify-between">'+
      '  <a class="document-link-a">' + escapeHtml(doc.name) + '</a>' +
@@ -588,6 +589,13 @@ function updateDocumentsList() {
     var elem = document.createElement('div');
     elem.className = 'list-group-item disabled';
     elem.textContent = gettext("There are no documents in this project yet.");
+    documents_list.insertBefore(elem, before);
+  }
+  else{
+    var elem = document.createElement('div');
+    elem.className = 'list-group-item total-documents';
+    elem.textContent = gettext("Total Documents: ") + entries.length;
+    
     documents_list.insertBefore(elem, before);
   }
   console.log("Documents list updated");
